86ec06854711e9c81649d8efeaa337de6d459dce
Ice <richard.jun.noh@gmail.com>
2020-02-19 00:38:26 -0600

    Add constructor and data storage variables
    Constructor with parameters for the real number 'a' and lateral
    number 'b' is added. The constructor also initializes the theta and
    radius of the given complex number.

--- src/ComplexNum.java
+++ src/ComplexNum.java
@@ -1,5 +1,38 @@
 
 public class ComplexNum
 {
-	
+	//immutable number
+	private double a;
+	private double b;
+
+	//calculated values for quick get
+	private double r;
+	private double t;
+
+	public ComplexNum(double a, double b)
+	{
+		this.a = a;
+		this.b = b;
+		r = setRadius();
+		t = setTheta();
+	}
+
+	private double setTheta()
+	{
+		return Math.tan(b / a); //TODO make sure this is correct for negative values
+	}
+
+	private double setRadius()
+	{
+		double aSquared = a * a;
+		double bSquared = b * b;
+
+		return Math.sqrt(aSquared + bSquared);
+	}
+
+	public static void main(String[] args)
+	{
+
+	}
+
 }
